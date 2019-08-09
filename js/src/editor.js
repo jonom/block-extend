2f8c4e2ab3cf29700a3dae3e49fb871f56d4a60a
Jonathon Menz <jonomenz@gmail.com>
2019-08-09 12:57:35 -0700

    Live update block number

--- js/src/editor.js
+++ js/src/editor.js
@@ -1,21 +1,25 @@
 const { createHigherOrderComponent } = wp.compose;
 const { Fragment } = wp.element;
-const { select } = wp.data;
 
-const getBlockNumber = (clientId) => {
+const getBlockNumber = ( clientId ) => {
 	// Got the block number for a given block ID, starting at 1
-	return select( 'core/editor' ).getBlockIndex(clientId) + 1;
-}
+	return wp.data.select( 'core/editor' ).getBlockIndex( clientId ) + 1;
+};
 
-const extendBlock = createHigherOrderComponent((BlockEdit) => {
-	return (props) => {
+const extendBlock = createHigherOrderComponent( ( BlockEdit ) => {
+	return wp.data.withSelect( ( select, props ) => {
+		return {
+			...props,
+			blockNumber: getBlockNumber( props.clientId ),
+		};
+	} )( ( props ) => {
 		return (
 			<Fragment>
-				<p><small><i>Block number {getBlockNumber(props.clientId)}</i></small></p>
-				<BlockEdit {...props} />
+				<p><small><i>Block number { props.blockNumber }</i></small></p>
+				<BlockEdit { ...props } />
 			</Fragment>
 		);
-	};
-}, "ExtendedBlock");
+	} );
+}, 'ExtendedBlock' );
 
-wp.hooks.addFilter('editor.BlockEdit', 'xwp/block-extend', extendBlock);
+wp.hooks.addFilter( 'editor.BlockEdit', 'xwp/block-extend', extendBlock );
