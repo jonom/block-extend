8639a8df7ac36a5bdf0ca1c4db310c9547fc5f40
Jonathon Menz <jonomenz@gmail.com>
2019-08-09 11:58:37 -0700

    Added block number
    * Block number is rendered but not automatically refreshed when new blocks are added

--- js/src/editor.js
+++ js/src/editor.js
@@ -1,15 +1,21 @@
 const { createHigherOrderComponent } = wp.compose;
 const { Fragment } = wp.element;
+const { select } = wp.data;
+
+const getBlockNumber = (clientId) => {
+	// Got the block number for a given block ID, starting at 1
+	return select( 'core/editor' ).getBlockIndex(clientId) + 1;
+}
 
 const extendBlock = createHigherOrderComponent((BlockEdit) => {
 	return (props) => {
 		return (
 			<Fragment>
-				<p><small><i>Block number goes here</i></small></p>
+				<p><small><i>Block number {getBlockNumber(props.clientId)}</i></small></p>
 				<BlockEdit {...props} />
 			</Fragment>
 		);
 	};
-}, "extendedBlock");
+}, "ExtendedBlock");
 
 wp.hooks.addFilter('editor.BlockEdit', 'xwp/block-extend', extendBlock);
