11de66416acf851f486f1eb9fffa6c9d6aaa6cb1
Jonathon Menz <jonomenz@gmail.com>
2019-08-09 14:55:43 -0700

    Bonus points?
    Show paragraph count in addition to block count

--- js/src/editor.js
+++ js/src/editor.js
@@ -4,16 +4,20 @@ const { Fragment } = wp.element;
 const numberedParagraphs = createHigherOrderComponent( ( BlockEdit ) => {
 	return wp.data.withSelect( ( select, props ) => {
 		// Add block index prop to every block (not ideal?)
+		const paragraphs = select( 'core/editor' ).getBlocks().filter( ( block ) => block.name === 'core/paragraph' );
+		const paragraphIndex = paragraphs.findIndex( ( block ) => block.clientId === props.clientId );
+		const paragraphNumber = ( paragraphIndex >= 0 ) ? paragraphIndex + 1 : null;
 		return {
 			...props,
 			blockNumber: select( 'core/editor' ).getBlockIndex( props.clientId ) + 1,
+			paragraphNumber,
 		};
 	} )( ( props ) => {
 		// Display index on paragraphs only
 		if ( props.name === 'core/paragraph' ) {
 			return (
 				<Fragment>
-					<p style={ { color: 'grey' } }><small><i>This is block number <b>{ props.blockNumber }</b> and it is a paragraph</i></small></p>
+					<p style={ { color: 'grey', margin: 0 } }><small><i>↓ This is block number <b>{ props.blockNumber }</b> and paragraph number <b>{ props.paragraphNumber }</b></i></small></p>
 					<BlockEdit { ...props } />
 				</Fragment>
 			);
