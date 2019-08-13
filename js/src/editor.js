c1071219e89c1218e05205d83b16a18def927d29
Jonathon Menz <jonomenz@gmail.com>
2019-08-13 10:47:30 -0700

    Refactor

--- js/src/editor.js
+++ js/src/editor.js
@@ -1,20 +1,26 @@
 const { createHigherOrderComponent } = wp.compose;
 const { Fragment } = wp.element;
 
-const numberedParagraphs = createHigherOrderComponent( ( BlockEdit ) => {
-	return wp.data.withSelect( ( select, props ) => {
-		// Add block index prop to every block (not ideal?)
+const BlockEditWithMetadata = ( BlockEdit ) => {
+	const thing = ( select, props ) => {
 		const paragraphs = select( 'core/editor' ).getBlocks().filter( ( block ) => block.name === 'core/paragraph' );
 		const paragraphIndex = paragraphs.findIndex( ( block ) => block.clientId === props.clientId );
 		const paragraphNumber = ( paragraphIndex >= 0 ) ? paragraphIndex + 1 : null;
 		const blockIndex = select( 'core/editor' ).getBlockIndex( props.clientId );
 		return {
-			...props,
-			blockNumber: blockIndex >= 0 ? blockIndex + 1 : null,
-			paragraphNumber,
+			blockMeta: {
+				blockNumber: blockIndex >= 0 ? blockIndex + 1 : null,
+				paragraphNumber,
+			},
 		};
-	} )( ( props ) => {
-		const { blockNumber, paragraphNumber } = props;
+	};
+
+	return wp.data.withSelect( thing )( BlockEdit );
+};
+
+const BlockEditWithParagraphNumbers = ( BlockEdit ) => {
+	return ( props ) => {
+		const { blockNumber, paragraphNumber } = props.blockMeta;
 		const originalBlock = <BlockEdit { ...props } />;
 		// Display index only on root paragraphs
 		if ( props.name === 'core/paragraph' && blockNumber && paragraphNumber ) {
@@ -26,7 +32,11 @@ const numberedParagraphs = createHigherOrderComponent( ( BlockEdit ) => {
 			);
 		}
 		return originalBlock;
-	} );
+	};
+};
+
+const numberRootParagraphs = createHigherOrderComponent( ( BlockEdit ) => {
+	return BlockEditWithMetadata( BlockEditWithParagraphNumbers( BlockEdit ) );
 }, 'NumberedParagraph' );
 
-wp.hooks.addFilter( 'editor.BlockEdit', 'xwp/numbered-paragraphs', numberedParagraphs );
+wp.hooks.addFilter( 'editor.BlockEdit', 'xwp/numbered-paragraphs', numberRootParagraphs );
